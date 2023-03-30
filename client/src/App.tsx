db952486fb39d2dfb8c856707298fc8570054f32
Chimezie Oji <chimezieoji@yahoo.com>
2023-03-30 19:02:14 -0400

    deployment change to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
 
       // save user credentials/profile to mongodb database
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {   // api endpoint connection to mongodb database
+        const response = await fetch('https://hellodiscover.onrender.com/api/v1/users', {   // api endpoint connection to mongodb database
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -129,7 +129,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://hellodiscover.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
